33319c269da3105152161de284d372f6646c45a7
Jarimus <jari.n.ahonen@gmail.com>
2025-01-20 15:34:54 +0200

    implement HTMLNode

---
+++ src/htmlnode.py
@@ -0,0 +1,30 @@
+class HTMLNode():
+    def __init__(self, tag: str =None, value: str =None, children: list =None, props: dict =None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def __repr__(self):
+        return f"HTMLNode('{self.tag}', '{self.value}', {self.children}, {self.props})"
+    
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if not self.props:
+            return None
+        result = ""
+        for attribute, value in self.props.items():
+            result += f' {attribute}="{value}"'
+        return result
+
+
+
+
+if __name__ == '__main__':
+    test = HTMLNode("<a>", "eeeet", ["div", "span"], {"href": "www.test.site", "target":"_blank"})
+    repr = test.__repr__()
+    print(repr)
+    test2 = HTMLNode('<a>', 'eeeet', ['div', 'span'], {'href': 'www.test.site', 'target': '_blank'})
+    print(test.__repr__() == test2.__repr__())
---
+++ src/test_htmlnode.py
@@ -0,0 +1,22 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHtmlNode(unittest.TestCase):
+    def test_props1(self):
+        node = HTMLNode("<p>", "a test paragraph", None, {"class": "main-paragraphs", "id": "first-paragraph"})
+        prop_target = ' class="main-paragraphs" id="first-paragraph"'
+        self.assertEqual(node.props_to_html(), prop_target, f"want {prop_target},\ngot {node.props_to_html()}")
+    
+    def test_props2(self):
+        node = HTMLNode("<p>", "a test paragraph")
+        prop_target = None
+        self.assertEqual(node.props_to_html(), prop_target)
+    
+    def test_props3(self):
+        node = HTMLNode("<p>", "a test paragraph", None, {"id": "introduction"})
+        prop_target = ' id="introduction"'
+        self.assertEqual(node.props_to_html(), prop_target, f"want {prop_target},\ngot {node.props_to_html()}")
+
+if __name__ == '__main__':
+     unittest.main()
