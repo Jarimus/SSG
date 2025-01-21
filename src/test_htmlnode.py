34ba41ea4e28591742bdaa97df4f4ad0dcdb2f88
Jarimus <jari.n.ahonen@gmail.com>
2025-01-21 05:27:16 +0200

    Fixed some tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -6,17 +6,17 @@ class TestHtmlNode(unittest.TestCase):
     def test_props1(self):
         node = HTMLNode("<p>", "a test paragraph", None, {"class": "main-paragraphs", "id": "first-paragraph"})
         prop_target = ' class="main-paragraphs" id="first-paragraph"'
-        self.assertEqual(node.props_to_html(), prop_target, f"want {prop_target},\ngot {node.props_to_html()}")
+        self.assertEqual(node.props_to_html(), prop_target, f"\nWANT {prop_target},\nGOT {node.props_to_html()}")
     
     def test_props2(self):
         node = HTMLNode("<p>", "a test paragraph")
-        prop_target = None
-        self.assertEqual(node.props_to_html(), prop_target)
+        prop_target = ""
+        self.assertEqual(node.props_to_html(), prop_target, f"\nWANT {prop_target},\nGOT {node.props_to_html()}")
     
     def test_props3(self):
         node = HTMLNode("<p>", "a test paragraph", None, {"id": "introduction"})
         prop_target = ' id="introduction"'
-        self.assertEqual(node.props_to_html(), prop_target, f"want {prop_target},\ngot {node.props_to_html()}")
+        self.assertEqual(node.props_to_html(), prop_target, f"\nWANT {prop_target},\nGOT {node.props_to_html()}")
 
 if __name__ == '__main__':
      unittest.main()
--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -5,15 +5,18 @@ from leafnode import LeafNode
 class TestLeafNode(unittest.TestCase):
     def test_LeafNodeNoProps(self):
        node = LeafNode("p", "This is a paragraph of text.")
-       self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+       target = "<p>This is a paragraph of text.</p>"
+       self.assertEqual(node.to_html(), target, f"\nWANT {target},\ngot {node.to_html()}")
 
     def test_LeafNodeProps(self):
        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-       self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+       target = '<a href="https://www.google.com">Click me!</a>'
+       self.assertEqual(node.to_html(), target, f"\nWANT {target},\ngot {node.to_html()}")
 
     def test_LeafNodeValueOnly(self):
         node = LeafNode(None, "This is a test value")
-        self.assertEqual(node.to_html(), "This is a test value")
+        target = "This is a test value"
+        self.assertEqual(node.to_html(), target, f"\nWANT {target},\ngot {node.to_html()}")
 
     def test_LeafNodeNoValue(self):
         node = LeafNode("a", None)
