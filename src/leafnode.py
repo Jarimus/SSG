52dbc208cffa90105a43ccd668fc72f4bf1f0e19
Jarimus <jari.n.ahonen@gmail.com>
2025-01-22 06:12:05 +0200

    Implement TextNode to HTMLNode

--- src/leafnode.py
+++ src/leafnode.py
@@ -3,6 +3,9 @@ from htmlnode import HTMLNode
 class LeafNode(HTMLNode):
     def __init__(self, tag: str, value: str, props: dict = None):
         super().__init__(tag, value, None, props)
+
+    def __eq__(self, other):
+        return self.tag == other.tag and self.value == other.value and self.props == other.props
     
     def to_html(self):
         if self.value == None:
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from leafnode import LeafNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq1(self):
@@ -11,12 +12,42 @@ class TestTextNode(unittest.TestCase):
     def test_eq2(self):
         node1 = TextNode("This is a test", TextType.BOLD)
         node2 = TextNode("Is this a test?", TextType.BOLD)
-        self.assertEqual(node1, node2)
+        self.assertNotEqual(node1, node2)
     
     def test_nq1(self):
         node1 = TextNode("This is a test", TextType.BOLD)
         node2 = TextNode("This is a test", TextType.TEXT)
         self.assertNotEqual(node1, node2)
+
+    def test_TEXT_to_html(self):
+        text_node = TextNode("Text", TextType.TEXT)
+        target = LeafNode(None, "Text")
+        self.assertEqual(text_node_to_html_node(text_node), target)
+
+    def test_BOLD_to_html(self):
+        text_node = TextNode("Some very bold text", TextType.BOLD)
+        target = LeafNode("b", "Some very bold text")
+        self.assertEqual(text_node_to_html_node(text_node), target)
+
+    def test_ITALIC_to_html(self):
+        text_node = TextNode("Some Italian text", TextType.ITALIC)
+        target = LeafNode("i", "Some Italian text")
+        self.assertEqual(text_node_to_html_node(text_node), target)
+
+    def test_CODE_to_html(self):
+        text_node = TextNode("L33t code", TextType.CODE)
+        target = LeafNode("code", "L33t code")
+        self.assertEqual(text_node_to_html_node(text_node), target)
+
+    def test_LINK_to_html(self):
+        text_node = TextNode("Link anchor text", TextType.LINK, "www.website.io")
+        target = LeafNode("a", "Link anchor text", {"href": text_node.url})
+        self.assertEqual(text_node_to_html_node(text_node), target)
+
+    def test_IMAGE_to_html(self):
+        text_node = TextNode("Alt text", TextType.IMAGE)
+        target = LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+        self.assertEqual(text_node_to_html_node(text_node), target)
 
 
 if __name__ == '__main__':
